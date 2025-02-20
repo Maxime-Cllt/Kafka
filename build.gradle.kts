b51d2320cabf2b82b13bee0bfaced9ee970cc524
Rahman Yilmaz <85690773+Sudo-Rahman@users.noreply.github.com>
2025-02-20 09:50:05 +0100

    gradleeee

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.kafka"
@@ -10,10 +11,24 @@ repositories {
 }
 
 dependencies {
-    implementation ("org.apache.kafka:kafka-clients:3.6.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("org.apache.kafka:kafka-clients:3.7.1")
+    implementation ("log4j:log4j:1.2.17")
+
 }
+
+
+tasks.register("runProducer", JavaExec::class) {
+    mainClass.set("org.kafka.Producer")
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
+tasks.register("runConsumer", JavaExec::class) {
+    mainClass.set("org.kafka.Consumer")
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
 
 tasks.test {
     useJUnitPlatform()
